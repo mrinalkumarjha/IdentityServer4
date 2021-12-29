b4c72983f50dfe1ac82301256264db2747bcbb38
Mrinal Kumar <Mrinal.Kumar@BritishCouncil.Org>
2021-12-29 19:26:32 +0530

    Configued openid in client

--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Movies.Client.ApiServices;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace Movies.Client
 {
@@ -27,6 +29,45 @@ namespace Movies.Client
             services.AddControllersWithViews();
 
             services.AddScoped<IMovieApiService, MovieApiService>();
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = "https://localhost:5005";
+
+                    options.ClientId = "movies_mvc_client";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    //options.Scope.Add("address");
+                    //options.Scope.Add("email");
+                    //options.Scope.Add("roles");
+
+                    //options.ClaimActions.DeleteClaim("sid");
+                    //options.ClaimActions.DeleteClaim("idp");
+                    //options.ClaimActions.DeleteClaim("s_hash");
+                    //options.ClaimActions.DeleteClaim("auth_time");
+                    //options.ClaimActions.MapUniqueJsonKey("role", "role");
+
+                    options.Scope.Add("movieAPI");
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    //options.TokenValidationParameters = new TokenValidationParameters
+                    //{
+                    //    NameClaimType = JwtClaimTypes.GivenName,
+                    //    RoleClaimType = JwtClaimTypes.Role
+                    //};
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +87,7 @@ namespace Movies.Client
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- README.md
+++ README.md
@@ -165,6 +165,59 @@ Identity server can also manage API access control. pictorial representation of 
 	Now if you run your project .. your identity server ui will appear .
 
 
+	you can login into id server using mrinal as id and mrinal as pass.
+
+
+
+# Add authentication layer in Movies.Client app.
+
+	1> in Movies.Client we need to add openid connect nuget package (Microsoft.AspNetCore.Authentication.OpenIdConnect).
+	2> Now we can configure openid connect in client app.
+		  services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = "https://localhost:5005";
+
+                    options.ClientId = "movies_mvc_client";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    //options.Scope.Add("address");
+                    //options.Scope.Add("email");
+                    //options.Scope.Add("roles");
+
+                    //options.ClaimActions.DeleteClaim("sid");
+                    //options.ClaimActions.DeleteClaim("idp");
+                    //options.ClaimActions.DeleteClaim("s_hash");
+                    //options.ClaimActions.DeleteClaim("auth_time");
+                    //options.ClaimActions.MapUniqueJsonKey("role", "role");
+
+                    options.Scope.Add("movieAPI");
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    //options.TokenValidationParameters = new TokenValidationParameters
+                    //{
+                    //    NameClaimType = JwtClaimTypes.GivenName,
+                    //    RoleClaimType = JwtClaimTypes.Role
+                    //};
+                });
+ 
+
+
+
+
+		
+
+
 	
 
 
